ee8f9ba839487283d2f6280394e245ea65ab2061
Lucas Monteiro de Sousa <107234259+LMsousa20@users.noreply.github.com>
2022-07-29 02:31:57 -0300

    alterando aqui tbm

--- src/pages/form/Form.js
+++ src/pages/form/Form.js
@@ -3,7 +3,7 @@ import './form.css'
 function Form(){
     return(
         <div className="agnd">
-        <iframe className="frm" src="https://agendamentos-8wu66q3n2-lmsousa20.vercel.app/"></iframe>
+        <iframe className="frm" src="https://agendamentos-ly9wkukxq-lmsousa20.vercel.app/"></iframe>
         </div>
     )
 }
